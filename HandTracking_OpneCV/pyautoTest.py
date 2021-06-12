5c541dc296bbd89a34b5fd6abc124c61d304c12b
mein0121 <45719589+mein0121@users.noreply.github.com>
2021-06-12 20:22:38 +0900

    pyauto

--- HandTracking_OpneCV/pyautoTest.py
+++ HandTracking_OpneCV/pyautoTest.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 import datetime
 
-# print(pyautogui.position())
+print(pyautogui.position())
 # pyautogui.moveTo(1652,359) # 마우스 움직임 (x,y, 시간)
 
 # pyautogui.moveRel(0,300,2) # 현재위치에서 이동.
@@ -33,7 +33,7 @@ p = ['a', 'b', 'c', 'd']
 
 # pyautogui.confirm(text='ss', title='aa', buttons=['OK', 'Cancel'])
 
-
+#
 # t = datetime.datetime.now().strftime("%Y-%M-%d %H-%M-%S")
 # print(t)
 # im = pyautogui.screenshot('{}.png'.format(t))
@@ -46,13 +46,30 @@ p = ['a', 'b', 'c', 'd']
 # print(button7Center)
 #
 # pyautogui.moveTo('calc7key.png')# shortcut version
-
-print('Press Ctrl-C to quit.')
 try:
-    while True:
-        x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-        print(positionStr, end='')
-        print('\b' * len(positionStr), end='', flush=True)
-except KeyboardInterrupt:
-    print('\n')
+    # pyautogui.leftClick('calc7key.png')
+    # pyautogui.leftClick('youtube.png')
+    # pyautogui.leftClick('seoulnews.png')
+    pyautogui.leftClick('naver.png')
+except TypeError:
+    print("Image not found")
+
+# print('Press Ctrl-C to quit.')
+# try:
+#     while True:
+#         x, y = pyautogui.position()
+#         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+#         print(positionStr, end='')
+#         print('\b' * len(positionStr), end='', flush=True)
+# except KeyboardInterrupt:
+#     print('\n')
+
+# pyautogui.press('win')
+# time.sleep(0.1)
+# pyautogui.write('chrome')
+# time.sleep(0.1)
+# pyautogui.press('enter')
+# time.sleep(0.1)
+# pyautogui.write('naver.com')
+# time.sleep(0.1)
+# pyautogui.press('enter')
