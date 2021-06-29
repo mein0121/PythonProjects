bcaa7ce59f7885dd2de5d6772fd20acc365bc999
mein0121 <45719589+mein0121@users.noreply.github.com>
2021-06-29 22:44:42 +0900

    Addpyqt

---
+++ DryHand/mmmm.py
@@ -0,0 +1,17 @@
+import testing
+
+
+# canvas = "T"
+# draw_arr = "m"
+# label = "svm"
+# testing.save_file(canvas,draw_arr)
+
+# testing.save_file(canvas, draw_arr, label)
+
+# testing.save_file(canvas,draw_arr)
+
+# testing.save_file(canvas, draw_arr, label)
+
+con = [['t','f'],'a','b','vc']
+
+print(con[1], con[0])
